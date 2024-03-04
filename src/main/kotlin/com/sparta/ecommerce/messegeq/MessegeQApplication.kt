a3473d1a41879d329d285a29994d406a83ce7b7e
bheejun <bhjun95@gmail.com>
2024-03-04 09:33:38 +0900

    [ADD] initial commit

---
+++ src/main/kotlin/com/sparta/ecommerce/messegeq/MessegeQApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.ecommerce.messegeq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessegeQApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessegeQApplication>(*args)
+}
---
+++ src/test/kotlin/com/sparta/ecommerce/messegeq/MessegeQApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sparta.ecommerce.messegeq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessegeQApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
